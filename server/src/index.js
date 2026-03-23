0928369e1641a0d07986c1667eef892f1e8b9c7f
Adrián Marín Hurtado <amarinhurtado@gmail.com>
2026-03-23 11:58:49 +0100

    Fase C Ejercicio 3

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,16 @@ app.use(express.json());
 
 app.use('/api/v1/tasks', taskRoutes);
 
+// Middleware global de errores (siempre al final)
+app.use((err, req, res, next) => {
+  if (err.message === 'NOT_FOUND') {
+    return res.status(404).json({ error: 'Recurso no encontrado' });
+  }
+
+  console.error(err);
+  res.status(500).json({ error: 'Error interno del servidor' });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
